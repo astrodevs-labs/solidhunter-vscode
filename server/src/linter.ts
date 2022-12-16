42b63b9d63120323debfc53f7151bd3c3c9316a6
Tristan Gremont <tristan.gremont@epitech.eu>
2022-12-16 11:01:09 +0100

    feat(lsp): added linter call to server

--- server/src/linter.ts
+++ server/src/linter.ts
@@ -1,8 +1,13 @@
 import ffi = require('ffi-napi');
 import ref = require('ref-napi');
 //import StructType = require('ref-struct-napi');
-var ArrayType = require('ref-array-di')(ref);
-var StructType = require('ref-struct-di')(ref);
+const ArrayType = require('ref-array-di')(ref);
+const StructType = require('ref-struct-di')(ref);
+import {
+	Diagnostic,
+	DiagnosticSeverity,
+  Range,
+} from 'vscode-languageserver/node';
 
 // Initialize the C-like data struct
 const VSSolid_Diag = StructType({
@@ -31,14 +36,42 @@ const lib = ffi.Library('linter-wrapper/target/debug/liblinter_wrapper', {
 
 export default class Linter {
     public static lintFile(filePath: string) {
+      const diagnostics : Diagnostic[] = [];
         // Call the library
-        const result = lib.lint_file(filePath);
-        // convert or return ? 
+      const result = lib.lint_file(filePath);
+      // convert or return ?
+          
+      result.array.forEach((elem : typeof VSSolid_Diag) => {
+        if (!elem.empty)
+        {
+            const diag : Diagnostic = {
+                severity: elem.severity,
+                range: Range.create(elem.start_line, elem.start_character, elem.end_line, elem.end_character),
+                message: elem.message,
+                source: 'solidhunter'
+            };
+          diagnostics.push(diag);
+        }
+      });
+      return diagnostics;
     }
 
     public static lintContent(path: string, content: string) {
+        const diagnostics : Diagnostic[] = [];
         // Call the library
         const result = lib.lint_content(path, content);
-        // convert or return ?
+        result.array.forEach((elem : typeof VSSolid_Diag) => {
+          if (!elem.empty)
+          {
+            const diag : Diagnostic = {
+                severity: elem.severity,
+                range: Range.create(elem.start_line, elem.start_character, elem.end_line, elem.end_character),
+                message: elem.message,
+                source: 'solidhunter'
+            };
+            diagnostics.push(diag);
+          }
+        });
+        return diagnostics;
     }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -22,6 +22,7 @@ import {
 import {
 	TextDocument
 } from 'vscode-languageserver-textdocument';
+import Linter from './linter';
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -144,11 +145,21 @@ documents.onDidClose(e => {
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
-	connection.console.error("kappa");
-	validateTextDocument(change.document);
+	connection.console.log("changed document");
+});
+
+documents.onDidSave(file => {
+	connection.console.log("saved document");
+	validateTextDocument(file.document);
+});
+
+documents.onDidOpen(file => {
+	connection.console.log("opened document");
+	validateTextDocument(file.document);
 });
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+	connection.console.log("Validating document with uri: '" + textDocument.uri +"' ...");
 	// In this simple example we get the settings for every validate run.
 	const settings = await getDocumentSettings(textDocument.uri);
 
@@ -157,40 +168,8 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 	const pattern = /\b[A-Z]{2,}\b/g;
 	let m: RegExpExecArray | null;
 
-	let problems = 0;
-	const diagnostics: Diagnostic[] = [];
-	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-		problems++;
-		const diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
-			range: {
-				start: textDocument.positionAt(m.index),
-				end: textDocument.positionAt(m.index + m[0].length)
-			},
-			message: `${m[0]} is all uppercase.`,
-			source: 'ex'
-		};
-		if (hasDiagnosticRelatedInformationCapability) {
-			diagnostic.relatedInformation = [
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Spelling matters'
-				},
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Particularly for names'
-				}
-			];
-		}
-		diagnostics.push(diagnostic);
-	}
-
+	const diagnostics: Diagnostic[] = Linter.lintFile(textDocument.uri.replace('file://', ''));
+	
 	// Send the computed diagnostics to VSCode.
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
