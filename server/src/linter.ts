529a29fdc1e03a03ae8201b09bfd247bcf6749e7
Alexandre jublot <alexjublot@gmail.com>
2022-12-16 11:14:28 +0100

    feat(linter): implementated asked todo about types

--- server/src/linter.ts
+++ server/src/linter.ts
@@ -1,25 +1,44 @@
 import ffi = require('ffi-napi');
 import ref = require('ref-napi');
-import StructType = require('ref-struct-napi');
-import ArrayType = require('ref-array-napi');
-
-// Initialize the C-like array
-const OutputArrayType = ArrayType(ref.types.int64, 2);
-
-// TODO: set correct output type
+//import StructType = require('ref-struct-napi');
+var ArrayType = require('ref-array-di')(ref);
+var StructType = require('ref-struct-di')(ref);
 
 // Initialize the C-like data struct
-const OutputType = StructType({
-  result: ref.types.int64,
-  operands: OutputArrayType,
-  description: ref.types.CString
+const VSSolid_Diag = StructType({
+    empty: ref.types.bool,
+    start_line: ref.types.int64,
+    start_char: ref.types.int64,
+    end_line: ref.types.int64,
+    end_char: ref.types.int64,
+    severity: ref.types.int64,
+    message: ref.types.CString
 });
+
+// Initialize the C-like array
+const DiagArray = ArrayType(VSSolid_Diag, 100);
+
+
+
 
 
 // Accessing the library
 // See its signature https://github.com/node-ffi/node-ffi/wiki/Node-FFI-Tutorial#signature
 const lib = ffi.Library('linter-wrapper/target/debug/liblinter_wrapper', {
-	lint_file: ['int', ['int']] // TODO: set correct function typing
+    lint_file: [DiagArray, ['string']],
+    lint_content: [DiagArray, ['string', 'string']]
 });
 
-//TODO: export calls to be used in server.ts
+export default class Linter {
+    public static lintFile(filePath: string) {
+        // Call the library
+        const result = lib.lint_file(filePath);
+        // convert or return ? 
+    }
+
+    public static lintContent(path: string, content: string) {
+        // Call the library
+        const result = lib.lint_content(path, content);
+        // convert or return ?
+    }
+}
